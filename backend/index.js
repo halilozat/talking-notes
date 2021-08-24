005598845389c3aa28e7daa1f0731612362d390e
halilozat <halilozat@hotmail.com>
2021-08-24 13:19:56 +0300

    Connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,18 @@
-import express from "express";
-import mongoose from "mongoose";
+import express from 'express'
+import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
+
+const app = express()
+dotenv.config()
+
+app.listen(process.env.PORT, () => {
+    mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+    })
+        .then(() => console.log('connected to database'))
+        .catch((err) => console.log(err))
+})
